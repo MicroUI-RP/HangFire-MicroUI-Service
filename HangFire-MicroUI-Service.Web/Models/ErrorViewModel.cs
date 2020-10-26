28fa8e404c8c8954a386b996e5e275e3e92a0f5a
Prasad Ravan <prasadcravan@gmail.com>
2020-10-26 21:53:32 +0530

    Initial Commit

---
+++ HangFire-MicroUI-Service.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HangFire_MicroUI_Service.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
